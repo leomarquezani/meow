e83098254ca79783c56f378428bbf10e39479ae5
Leo <leomarquezani@gmail.com>
2018-09-20 21:36:53 -0300

    Added repository

--- db/repository.go
+++ db/repository.go
@@ -8,6 +8,24 @@ import (
 
 type Repository interface {
 	Close()
-	InsertMeow(ctx context.Context, meow schema.Meow)
-	ListMeows()
+	InsertMeow(ctx context.Context, meow schema.Meow) error
+	ListMeows(ctx context.Context, skip uint64, take uint64) ([]schema.Meow, error)
+}
+
+var impl Repository
+
+func SetRepository(repository Repository) {
+	impl = repository
+}
+
+func Close() {
+	impl.Close
+}
+
+func InsertMeow(ctx context.Context, meow schema.Meow) error {
+	return impl.InsertMeow(ctx, meow)
+}
+
+func ListMeows(ctx context.Context, skip uint64, take uint64) ([]schema.Meow, error) {
+	return impl.ListMeows(ctx, skip, take)
 }
